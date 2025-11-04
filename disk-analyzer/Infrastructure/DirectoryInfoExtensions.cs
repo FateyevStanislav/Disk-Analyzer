dd9bca97b77939399ab576afdb5bc8de9a96765e
FateyevStanislav <fateev0803@gmail.com>
2025-11-04 15:06:41 +0500

    added directory extensions, value type, entity

---
+++ disk-analyzer/Infrastructure/DirectoryInfoExtensions.cs
@@ -0,0 +1,29 @@
+﻿namespace DiskAnalyzer.Infrastructure;
+
+public static class DirectoryInfoExtensions
+{
+    public static List<FileSystemInfo> GetFilesSortedby<TSelected>(
+        this DirectoryInfo directory, 
+        Func<FileSystemInfo, TSelected> selector, 
+        bool reversed = false)
+        where TSelected : IComparable
+    {
+        var files = directory.EnumerateFileSystemInfos().ToList();
+        files.Sort((x, y) => selector(x).CompareTo(selector(y)) * (reversed ? -1 : 1));
+        return files;
+    }
+
+    public static List<FileSystemInfo> GetFilesWhere(
+        this DirectoryInfo directory,
+        Func<FileSystemInfo, bool> predicate)
+    {
+        return directory.EnumerateFileSystemInfos().Where(f => predicate(f)).ToList();
+    }
+
+    public static IEnumerable<IGrouping<TKey, FileSystemInfo>> GetFilesGroupedBy<TKey>(
+        this DirectoryInfo directory,
+        Func<FileSystemInfo, TKey> keySelector)
+    {
+        return directory.EnumerateFileSystemInfos().GroupBy(f => keySelector(f));
+    }
+}
---
+++ disk-analyzer/Program.cs
@@ -0,0 +1,26 @@
+﻿using DiskAnalyzer.Infrastructure;
+
+namespace DiskAnalyzer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var direct = new DirectoryInfo("C:\\Users\\Станислав\\Downloads");
+        var res1 = direct.GetFilesSortedby(f => f.Extension);
+        foreach (var file in res1)
+            Console.WriteLine(file.FullName);
+        Console.WriteLine();
+        var res2 = direct.GetFilesWhere(f => f.LastAccessTime < new DateTime(2025, 11, 3));
+        foreach (var file in res1)
+            Console.WriteLine(file.FullName);
+        Console.WriteLine();
+        var res3 = direct.GetFilesGroupedBy(f => f.Extension);
+        foreach (var group in res3)
+        {
+            Console.WriteLine(group.Key);
+            foreach (var file in group)
+                Console.WriteLine(file);
+        }
+    }
+}
