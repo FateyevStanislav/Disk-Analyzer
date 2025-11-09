c20285110efed76f661d9c594984b7d64ed2c916
FateyevStanislav <fateev0803@gmail.com>
2025-11-09 21:32:59 +0500

    class and counter method signature

---
+++ DiskAnalyzer/Domain/FileWeigher.cs
@@ -0,0 +1,9 @@
+﻿namespace DiskAnalyzer.Domain;
+
+public class FileWeigher
+{
+    public int CountFiles(string directoryName, int maxDepth, Predicate<FileSystemInfo> predicate)
+    {
+        throw new NotImplementedException();
+    }
+}
