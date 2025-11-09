b261101f4662a3e546da164929d8b229e4d44515
oxxxyarik <yarosla.a.r@gmail.com>
2025-11-09 23:19:01 +0500

    Added WeightingRecord entity, Repository, Repository Tests

---
+++ DiskAnalyzer/Domain/WeightingRecordRepository.cs
@@ -0,0 +1,45 @@
+﻿using DiskAnalyzer.Domain;
+using System.Collections.Concurrent;
+
+namespace DiskAnalyzer.Domain
+{
+    public static class WeightingRecordRepository
+    {
+        private static readonly ConcurrentDictionary<Guid, WeightingRecord> Repository = new();
+
+        public static int Count => Repository.Count;
+
+        public static void AddRecord(WeightingRecord weightingRecord)
+        {
+            if (!Repository.TryAdd(weightingRecord.Id, weightingRecord))
+            {
+                throw new ArgumentException("Exception in AddRecord Method");
+            }
+            Console.WriteLine($"Successfully added new {weightingRecord.ToString()}");
+        }
+
+        public static WeightingRecord GetRecord(Guid Id)
+        {
+            if (!Repository.TryGetValue(Id, out WeightingRecord weightingRecord))
+            {
+                throw new ArgumentException("Exception in GetRecord Method");
+            }
+            Console.WriteLine($"Successfully got value of {weightingRecord.ToString()}");
+            return weightingRecord;
+        }
+
+        public static void RemoveRecord(Guid Id)
+        {
+            if (!Repository.TryRemove(Id, out WeightingRecord weightingRecord))
+            {
+                throw new ArgumentException("Exception in RemoveRecord Method");
+            }
+            Console.WriteLine($"Successfully removed {weightingRecord.ToString()}");
+        }
+
+        public static void Clear()
+        {
+            Repository.Clear();
+        }
+    }
+}
