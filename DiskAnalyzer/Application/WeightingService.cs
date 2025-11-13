750ae30a7ecb3369cb4ffb72e913ee0ac10656d4
yabra <35022445+Yabra@users.noreply.github.com>
2025-11-13 13:09:45 +0500

    Minimal API

---
+++ DiskAnalyzer/Application/WeightingService.cs
@@ -0,0 +1,59 @@
+﻿using DiskAnalyzer.Domain;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace disk_analyzer.Application
+{
+    public record RequestDto(string Path, int MaxDepth);
+
+    internal class WeightingService
+    {
+        public static void InitService(WebApplication app)
+        {
+            app.MapPost("/weightings",
+                (RequestDto dto) =>
+                {
+                    var id = StartWeighting(dto.Path, dto.MaxDepth);
+                    return Results.Created($"/weightings/{id}", new { id });
+                }
+                );
+
+            app.MapGet("/weightings/{id:guid}",
+                (Guid id) =>
+                {
+                    try
+                    {
+                        var record = GetWeighting(id);
+                        return Results.Ok(record);
+                    }
+                    catch (Exception ex)
+                    {
+                        return Results.NotFound(ex);
+                    }
+                }
+                );
+        }
+
+        public static Guid StartWeighting(string path, int maxDepth)
+        {
+            var id = Guid.NewGuid();
+
+            try
+            {
+                var count = FileWeigher.CountFiles(path, maxDepth);
+                var newRecord = new WeightingRecord(id, path, maxDepth, count, null);
+                WeightingRecordRepository.AddRecord(newRecord);
+            }
+
+            catch (Exception ex)
+            {
+                var newRecord = new WeightingRecord(id, path, maxDepth, 0, ex.Message);
+                WeightingRecordRepository.AddRecord(newRecord);
+            }
+
+            return id;
+        }
+
+        public static WeightingRecord? GetWeighting(Guid guid) => WeightingRecordRepository.GetRecord(guid);
+    }
+}
--- DiskAnalyzer/Program.cs
+++ DiskAnalyzer/Program.cs
@@ -1,4 +1,6 @@
-﻿using NUnitLite;
+﻿using disk_analyzer.Application;
+using Microsoft.AspNetCore.Builder;
+using NUnitLite;
 
 namespace DiskAnalyzer;
 
@@ -7,5 +9,9 @@ public class Program
     public static void Main(string[] args)
     {
         new AutoRun().Execute(args);
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+        WeightingService.InitService(app);
+        app.Run();
     }
 }
