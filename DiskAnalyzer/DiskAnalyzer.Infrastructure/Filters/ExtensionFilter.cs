8e04fe3a18eaea040160134cbe15bdef37281a6a
FateyevStanislav <fateev0803@gmail.com>
2025-12-14 14:56:58 +0500

    fix(filters) ability to write extension both with dot and without

--- DiskAnalyzer/DiskAnalyzer.Infrastructure/Filters/ExtensionFilter.cs
+++ DiskAnalyzer/DiskAnalyzer.Infrastructure/Filters/ExtensionFilter.cs
@@ -4,10 +4,18 @@ using DiskAnalyzer.Domain.Attributes;
 namespace DiskAnalyzer.Infrastructure.Filters;
 
 [FilterType("Extension")]
-public class ExtensionFilter(string extension) : IFileFilter
+public class ExtensionFilter : IFileFilter
 {
     [FilterInfo("Extension")]
-    public string Extension { get; } = extension;
+    public string Extension { get; }
 
-    public bool ShouldInclude(FileInfo file) => $".{Extension}" == file.Extension;
+    public ExtensionFilter(string extension)
+    {
+        Extension = extension.StartsWith('.')
+            ? extension
+            : $".{extension}";
+    }
+
+    public bool ShouldInclude(FileInfo file) =>
+        file.Extension.Equals(Extension, StringComparison.OrdinalIgnoreCase);
 }
